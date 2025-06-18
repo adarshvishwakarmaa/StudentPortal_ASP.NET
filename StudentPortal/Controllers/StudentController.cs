c0f7a93f1bcad6a77cba6e8295b8b7146c3810ea
adarshvishwakarmaa <adarshvishwakarma2004@gmail.com>
2025-06-18 20:38:27 +0530

    Add project files.

---
+++ StudentPortal/Controllers/StudentController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentPortal.data;
+using StudentPortal.Models;
+using System.Reflection.Metadata.Ecma335;
+
+namespace StudentPortal.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly StudentDbContext context;
+        public  StudentController (StudentDbContext context)
+        {
+            this.context = context;
+        }
+   
+        public IActionResult Index()
+        {
+           var student = context.Students.ToList();
+            return View(student);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Student student) {
+            if (ModelState.IsValid)
+            {
+                {
+                    context.Students.Add(student);
+                    context.SaveChanges();
+                    return RedirectToAction("Index");
+                   
+
+                }
+            }
+            return View(student);
+
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int?id) {
+            if (id != null)
+            {
+                var student = context.Students.FirstOrDefault(a => a.Id == id);
+                if (student != null)
+                {
+                    return View(student);
+                }
+                
+            }
+            return RedirectToAction("Index");
+
+        }
+        [HttpPost]
+        public IActionResult Edit(Student student) {
+            if (student != null) {
+                context.Students.Update(student);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id) {
+            if (id != null)
+            {
+                var student = context.Students.FirstOrDefault(a => a.Id == id);
+                if (student != null)
+                {
+                    return View(student);
+                }
+               
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult Delete(Student student) {
+            if (student != null) {
+                context.Students.Remove(student);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Details(int? id)
+        {
+            if (id != null)
+            {
+                var student = context.Students.FirstOrDefault(a => a.Id == id);
+                if (student != null) {
+                    return View (student);
+                }
+              
+            }
+            return RedirectToAction("Index");   
+               
+        }
+       
+
+    }
+}
---
+++ StudentPortal/data/StudentDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentPortal.Models;
+
+namespace StudentPortal.data
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected StudentDbContext()
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+
+
+}
